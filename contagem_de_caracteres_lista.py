09649e568b5e800aa46ced947e0b29469e604254
Atila Dalan <63621795+atiladalan@users.noreply.github.com>
2020-04-30 14:27:55 -0300

    contagem_de_caracteres_lista.py

---
+++ contagem_de_caracteres_lista.py
@@ -0,0 +1,38 @@
+def contar_caracteres(s):
+    """ Função que conta os caracteres de uma string
+
+    Ex:
+
+    >>> contar_caracteres('renzo')
+    e:1
+    n:1
+    o:1
+    r:1
+    z:1
+    >>> contar_caracteres('banana')
+    a:3
+    b:1
+    n:2
+
+    :param s: string a ser contada
+    """
+    caracteres_ordenados = sorted(s)
+
+    caracterer_anterior = caracteres_ordenados[0]
+    contagem = 1
+
+    for caracter in caracteres_ordenados[1:]:
+        if caracter == caracterer_anterior:
+            contagem +=1
+        else:
+            print(f'{caracterer_anterior}: {contagem}')
+            caracterer_anterior = caracter
+            contagem = 1
+
+    print(f'{caracterer_anterior}: {contagem}')
+
+if __name__ == '__main__':
+    contar_caracteres('renzo')
+    print()
+    contar_caracteres('banana')
+
