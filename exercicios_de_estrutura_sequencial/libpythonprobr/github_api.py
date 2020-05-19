85e9d216018993cb03ae56f026f7218fb8f9591c
atiladalan <atiladalan@gmail.com>
2020-05-19 13:41:56 -0300

    Criado exemplo do arquivo github_api.py

---
+++ exercicios_de_estrutura_sequencial/libpythonprobr/github_api.py
@@ -0,0 +1,19 @@
+#from urllib import request
+
+import requests
+#from pip._vendor import requests
+from requests import request
+
+
+def buscar_avatar(usuario):
+	"""
+	Busca o avatar de um usuário no github
+	:param usuario: str com o nome de usuário no github
+	:return: str com o link do avatar
+	"""
+	url = f'https://api.github.com/users/{usuario}'
+	resp = requests.get(url)
+	return resp.json()['avatar_url']
+
+if __name__ == '__main__':
+    print(buscar_avatar('atiladalan'))
