1008d50afc95a2173bb5db9c9350f974b93ce7cf
atiladalan <atiladalan@gmail.com>
2020-05-18 22:24:12 -0300

    Feito o método de pegar informação do usuario para mostrar na tela

---
+++ exercicios_de_estrutura_sequencial/exercicio_2.py
@@ -0,0 +1,6 @@
+"""
+Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+"""
+
+numero = input('Digite o Numero desejado: ')  #input "Método para ser utilizado na captação de entrada com usuario"
+print('O número informado foi:', numero)
