8a5de2e5b730afb184574e823bf300caf700112d
atiladalan <atiladalan@gmail.com>
2020-05-18 23:34:47 -0300

    Executado o exercicio

---
+++ exercicios_de_estrutura_sequencial/exercicio_5.py
@@ -0,0 +1,8 @@
+"""
+Faça um Programa que converta metros para centímetros.
+"""
+
+metros= float(input('Digite os Metros a serem convertidos para "Centimetros":'))
+centimetros = metros * 100
+	
+print('Os Centimetros são:', centimetros, 'cm')
