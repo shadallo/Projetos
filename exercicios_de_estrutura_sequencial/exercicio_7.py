74567e8dd563b0c487d788ed3b48bb5501454ca1
atiladalan <atiladalan@gmail.com>
2020-06-15 21:41:12 -0300

    Aplicando exercicio_7.py

---
+++ exercicios_de_estrutura_sequencial/exercicio_7.py
@@ -0,0 +1,12 @@
+"""
+Faça um Programa que calcule a área de um quadrado,
+ em seguida mostre o dobro desta área para o usuário.
+
+"""
+
+lado = float(input('Digite o Lado do Quadrado:'))
+area = lado * lado
+dobro = area * 2
+
+print('A Área do quadrado é:', area)
+print('e seu Dobro é:', dobro)
