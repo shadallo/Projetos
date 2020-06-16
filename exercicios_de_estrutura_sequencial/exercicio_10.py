0d35116d9b1b70b8bb5f3e422735ece4d08742c7
atiladalan <atiladalan@gmail.com>
2020-06-15 22:17:21 -0300

    Aplicando exercicio_10.py

---
+++ exercicios_de_estrutura_sequencial/exercicio_10.py
@@ -0,0 +1,9 @@
+"""
+Faça um Programa que peça a temperatura em graus Celsius,
+transforme e mostre em graus Farenheit.
+"""
+
+temperatura = float(input('Digite a Temperatura:'))
+temp_celsius = ((temperatura * 9 / 5) + 32)
+
+print('A Temperatura em Farenheit é:', temp_celsius, 'ºF')
